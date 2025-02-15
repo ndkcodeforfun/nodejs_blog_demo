1ee6d119d594b52c271896d6698b49c1f6504439
Khanh Nguyen <nguyenkhanh3055@gmail.com>
2025-02-15 10:52:58 +0700

    test

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 // HTTP logger
-app.use
-(morgan('combined'));
+      app.use
+      (morgan('combined'));
 
 //Template engine
           app.engine('hbs', hbs.engine({ extname: '.hbs' }));
